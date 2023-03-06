1592608c3346f7d44e26414d3d3907e76b14465d
Armand Dogaru <dogaruionelarmand@gmail.com>
2023-03-06 12:23:06 +0200

    ENUM

---
+++ MoreOOP/src/main/java/com/endava/blackjack/Card.java
@@ -0,0 +1,38 @@
+package com.endava.blackjack;
+
+public class Card {
+    /**
+     * This class is modelling a deck of cards
+     */
+
+    private Suit suit;
+    private Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit; // this is the color/type
+        this.rank = rank; // value of the card
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public static void main(String[] args) {
+        Card card1 = new Card(Suit.CLUBS, Rank.JACK);
+        Card card2 = new Card(Suit.DIAMONDS, Rank.TEN);
+//        int total = card1.plus(card2);
+
+    }
+}
---
+++ MoreOOP/src/main/java/com/endava/blackjack/Suit.java
@@ -0,0 +1,11 @@
+package com.endava.blackjack;
+
+public enum Suit {
+    /**
+     * This is an enum that models the suits / color/type of a card
+     */
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES;
+}
